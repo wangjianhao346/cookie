52617e9596241ac64a62c7c7287adc74873d948d
wangjianhao <946669782@qq.com>
2018-11-11 14:27:35 +0800

    测试RESTful风格传单个值和传对象的两种用法

--- src/main/java/com/rain/cookie/controller/Index.java
+++ src/main/java/com/rain/cookie/controller/Index.java
@@ -5,10 +5,7 @@ import com.rain.cookie.entity.User;
 import com.rain.cookie.server.UserServer;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
@@ -19,10 +16,19 @@ public class Index {
     @Resource
     private UserServer userServer;
 
-    @ApiOperation("根据id查找用户信息")
+    @ApiOperation("根据User查找用户信息")
     @RequestMapping(method = RequestMethod.POST)
     public UserDTO test(@RequestBody User user) {
         System.out.println(userServer.getUser(user.getId()).toString());
+        System.out.println("test1");
         return userServer.getUser(user.getId());
+    }
+
+    @ApiOperation("根据id查找用户信息")
+    @RequestMapping(value = "{id}", method = RequestMethod.POST)
+    public UserDTO test2(@PathVariable("id") int id) {
+        System.out.println(userServer.getUser(id).toString());
+        System.out.println("test2");
+        return userServer.getUser(id);
     }
 }
